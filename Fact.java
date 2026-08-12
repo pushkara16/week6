b6d476549220788d9c6e26848e45acdc0743d342
Pushkara Singamaneni <pushkarachowdary16@gmail.com>
2026-08-12 09:44:51 +0530

    Implement factorial calculation in Fact.java

---
+++ Fact.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Fact {
+    static int fact(int n) {
+        int ans = 1;
+
+        for (int i = 1; i <= n; i++) {
+            ans *= i;
+        }
+
+        return ans;
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println("Factorial of " + n + " = " + Fact.fact(n));
+    }
+}
