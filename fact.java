2643a8db2f8830f43d862856de53006fff4dca60
Pushkara Singamaneni <pushkarachowdary16@gmail.com>
2026-08-12 09:34:39 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 
 class Factorial {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+static int fact(int n) {
+        int ans = 1;
 
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-
-        long factorial = 1;
-
-        if (n < 0) {
-            System.out.println("Factorial is not defined for negative numbers.");
-        } else {
-            for (int i = 1; i <= n; i++) {
-                factorial *= i;
-            }
-            System.out.println("Factorial of " + n + " is " + factorial);
+        for (int i = 1; i <= n; i++) {
+            ans *= i;
         }
 
-        sc.close();
+        return ans;
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+    }
+
 }
