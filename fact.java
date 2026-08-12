313ff104c86b71340a8cdb3488742ac1263732d0
Pushkara Singamaneni <pushkarachowdary16@gmail.com>
2026-08-12 09:39:51 +0530

    Rename Factorial class to Fact and update usage

--- fact.java
+++ fact.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
-
-class Factorial {
-static int fact(int n) {
+class Fact {
+    static int fact(int n) {
         int ans = 1;
 
         for (int i = 1; i <= n; i++) {
@@ -10,12 +9,14 @@ static int fact(int n) {
 
         return ans;
     }
-
+}
+class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter a number:");
         int n = sc.nextInt();
-        System.out.println(fact(n));
-    }
 
+        System.out.println(Fact.fact(n));
+    }
 }
